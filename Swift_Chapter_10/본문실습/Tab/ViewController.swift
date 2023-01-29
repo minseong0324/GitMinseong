3c34464d097b1fe1fd20f837f8b04b12e6683870
minseong0324 <65233075+minseong0324@users.noreply.github.com>
2023-01-29 20:32:42 +0900

    Add files via upload

---
+++ Swift_Chapter_10/본문실습/Tab/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Tab
+//
+//  Created by BeomGeun Lee on 2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
